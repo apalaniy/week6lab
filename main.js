6eeb43382acf1e8a5e7ccd0f5a810f60fb795412
apalaniy <apalaniy@andrew.cmu.edu>
2018-10-19 15:24:52 -0400

    changes

--- main.js
+++ main.js
@@ -22,4 +22,21 @@ function completeListItem(item) {
     }
 }
 
+$(document).ready(function(){
+    $("#add-item").click(function() { // bind handler for click event
+        var list = $("#grocery-list"); // get the ol list by id
+        var itemInput = $("#new-list-item"); // get the new item input
+      // append the input value within an li element
+        list.append("<li>" + itemInput.val() + " <button class='delete-item'>X</button></li>")
+        
+    $(".delete-item").click(function() {
+        $(this).parent().remove();
+        console.log("test");
+
+});
+ 
+    });
+});
+
+
 
